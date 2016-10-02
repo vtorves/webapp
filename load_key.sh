504eead1e3b119a0ab6d20c842c6f4e62b7d3a56
Mauricio Vieira <mauricio@mauriciovieira.net>
2016-10-01 22:00:23 -0300

    Add --global to git config

--- load_key.sh
+++ load_key.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
+git config --global user.name "Travis CI"
+git config --global user.email "$COMMIT_AUTHOR_EMAIL"
 
 openssl aes-256-cbc -K $encrypted_9ba9e0c1817c_key -iv $encrypted_9ba9e0c1817c_iv -in id_rsa_travis.enc -out id_rsa_travis -d
 chmod 600 id_rsa_travis
