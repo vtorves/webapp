2b3c62a1331cbadca4e5b8945c2f35c1bd3376ae
Mauricio Vieira <mauricio@mauriciovieira.net>
2016-10-01 21:51:16 -0300

    Simplify script

--- load_key.sh
+++ load_key.sh
@@ -3,8 +3,4 @@
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc --add -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in id_rsa_travis.enc -out ~\/.ssh/id_rsa_travis -d
+openssl aes-256-cbc --add -K $encrypted_9ba9e0c1817c_key -iv $encrypted_9ba9e0c1817c_iv -in id_rsa_travis.enc -out ~\/.ssh/id_rsa_travis -d
