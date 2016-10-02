7ede1eb9fee4c270d6e2b69db0ed06902991f9bc
Mauricio Vieira <mauricio@mauriciovieira.net>
2016-10-01 21:56:55 -0300

    Add ssh-agent command because --add option did not work

--- load_key.sh
+++ load_key.sh
@@ -3,4 +3,7 @@
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-openssl aes-256-cbc --add -K $encrypted_9ba9e0c1817c_key -iv $encrypted_9ba9e0c1817c_iv -in id_rsa_travis.enc -out ~\/.ssh/id_rsa_travis -d
+openssl aes-256-cbc -K $encrypted_9ba9e0c1817c_key -iv $encrypted_9ba9e0c1817c_iv -in id_rsa_travis.enc -out id_rsa_travis -d
+chmod 600 id_rsa_travis
+eval `ssh-agent -s`
+ssh-add id_rsa_travis
