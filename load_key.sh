e4e3ee65bbbb5d20f9bdc7c0dfe5e6baece2bd0b
Mauricio Vieira <mauricio@mauriciovieira.net>
2016-10-01 22:27:34 -0300

    Remove load_key.sh. Do everything on .travis.yml

--- load_key.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-git config --global user.name "Travis CI"
-git config --global user.email "$COMMIT_AUTHOR_EMAIL"
-
-openssl aes-256-cbc -K $encrypted_9ba9e0c1817c_key -iv $encrypted_9ba9e0c1817c_iv -in id_rsa_travis.enc -out id_rsa_travis -d
-chmod 600 id_rsa_travis
-eval `ssh-agent -s`
-ssh-add id_rsa_travis
